073932c0688bdbb9b8b7a5c34803e9c95e332048
siva-82 <118682661+siva-82@users.noreply.github.com>
2024-06-11 17:27:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import uploadRoutes from './routes/uploadRoutes.js'
 connectDB();
 const app=express()
 
-app.use(cors({credentials:true}))
+app.use(cors({{origin:["https://insta-mern-9zfs.onrender.com"],credentials:true}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
